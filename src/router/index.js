d533def2f95e59326147473ff838b48bf4952df3
Wangchengwu <wcw515@163.com>
2019-07-02 13:48:29 +0800

    阶段性提交》已无技术难点。

--- src/router/index.js
+++ src/router/index.js
@@ -5,21 +5,31 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 export default new VueRouter({
-  routes: [{
-    path: '/',
-    name: 'hello',
-    component: () => import('@/components/HelloWorld')
-  }, {
-    path: '/helloGd',
-    name: 'helloGd',
-    component: () => import('@/views/demo/HelloAMap')
-  }, {
-    path: '/test',
-    name: 'test',
-    component: () => import('@/views/demo/Test')
-  }, {
-    path: '/ll',
-    name: 'll',
-    component: () => import('@/views/demo/LabelsLayerDemo')
-  }]
+  routes: [
+    {
+      path: '/',
+      name: 'hello',
+      component: () => import('@/components/HelloWorld')
+    },
+    {
+      path: '/helloGd',
+      name: 'helloGd',
+      component: () => import('@/views/demo/HelloAMap')
+    },
+    {
+      path: '/otwTrackMap',
+      name: 'otwTrackMap',
+      component: () => import('@/components/OnTheWayTrackMap')
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: () => import('@/views/demo/Test')
+    },
+    {
+      path: '/ll',
+      name: 'll',
+      component: () => import('@/views/demo/LabelsLayerDemo')
+    }
+  ]
 })
